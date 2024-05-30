2fda3841a3f06372790c01c13e4c9a32936259a0
Daniel <daniel.patek@email.cz>
2024-05-30 22:09:51 +0200

    style: navbar mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <div className=" bg-base-300 fixed w-full">
             <div className="navbar max-w-7xl m-auto">
-                <div className="navbar-start">
+                <div className="navbar-start flex md:hidden">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -13,9 +13,11 @@ const NavBar = () => {
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 hidden"></ul>
                     </div>
+                    <CheckServiceStatusButton />
                 </div>
-                <div className="navbar-center flex gap-5">
-                    <span className="font-semibold hidden md:block">Leontyna cz/sk</span>
+                <div className="navbar-start hidden md:flex"></div>
+                <div className="navbar-center gap-5 hidden md:flex">
+                    <span className="font-semibold">Leontyna cz/sk</span>
                     <CheckServiceStatusButton />
                     <label className="flex cursor-pointer gap-2 items-center">
                         <svg
@@ -48,7 +50,39 @@ const NavBar = () => {
                         </svg>
                     </label>
                 </div>
-                <div className="navbar-end"></div>
+                <div className="navbar-end hidden md:flex"></div>
+                <div className="navbar-end flex md:hidden">
+                    <label className="flex cursor-pointer gap-2 items-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                        </svg>
+                        <input type="checkbox" value="light" className="toggle theme-controller" />
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <circle cx="12" cy="12" r="5" />
+                            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+                        </svg>
+                    </label>
+                </div>
             </div>
         </div>
     );
