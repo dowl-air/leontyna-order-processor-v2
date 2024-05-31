b01a14718f23ba6fe151ca7839f671822a526a87
Daniel <daniel.patek@email.cz>
2024-05-31 11:30:41 +0200

    feat: add cron job

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,10 +1,15 @@
 import { feedsDownload } from "@/actions/feedsDownload";
 import { sendOrders } from "@/actions/triggerOrdersSend";
+import { NextApiRequest } from "next";
+import { headers } from "next/headers";
 
 export const dynamic = "force-dynamic";
 
 export const GET = async () => {
     try {
+        const headersList = headers()
+        const auth = headersList.get('Authorization')
+        if (auth !== `Bearer ${process.env.CRON_SECRET}`) return Response.error();
         await feedsDownload();
         await sendOrders();
         return Response.json({ message: "Feeds downloaded and orders sent." });
