a427750602493e5fe0f7f09144c4d2ed7ed478df
dowl <daniel.patek@email.cz>
2024-10-15 15:37:43 +0200

    fix: add ip to env

--- actions/sendOrder.ts
+++ actions/sendOrder.ts
@@ -3,20 +3,18 @@
 import { KontriOrderToSend } from "@/types/KontriOrder";
 import { AddOrderResponse } from "@/types/api/AddOrderResponse";
 
-// todo add to .env
-const SEND_ORDER_URL = "http://34.75.56.113:8080/api/sendOrder"
+const SEND_ORDER_URL = process.env.GCP_SERVER_ADDRESS!;
 
 export const sendOrder = async (orderObject: KontriOrderToSend) => {
     try {
-        const resp = await fetch(SEND_ORDER_URL, { 
-            method: "POST", 
-            body: JSON.stringify(orderObject), 
-            headers: {"Content-Type": "application/json"} 
+        const resp = await fetch(SEND_ORDER_URL, {
+            method: "POST",
+            body: JSON.stringify(orderObject),
+            headers: { "Content-Type": "application/json" },
         });
-        return (await resp.json() as AddOrderResponse).result;
-    }
-    catch (error) {
+        return ((await resp.json()) as AddOrderResponse).result;
+    } catch (error) {
         console.log(error);
         return { Code: 0, Message: "Error while sending order." };
     }
-}
+};
